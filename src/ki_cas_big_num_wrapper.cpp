2ff4e13bc832173b986f023acb60c3442080eaeb
John Till <johndtill@gmail.com>
2026-01-04 20:41:20 +0000

    Try including GMP memory tracking initialisation before Flint

--- src/ki_cas_big_num_wrapper.cpp
+++ src/ki_cas_big_num_wrapper.cpp
@@ -277,7 +277,7 @@ static std::allocator<size_t> allocator;
 static std::unordered_set<const void*> allocated_memory;
 static std::shared_mutex allocation_mutex;
 
-static void* leakTrackingAlloc(size_t n) {
+void* __leakTrackingAlloc(size_t n) {
     allocation_mutex.lock_shared();
     size_t* allocated = allocator.allocate(n);
     if(allocated){
@@ -289,28 +289,22 @@ static void* leakTrackingAlloc(size_t n) {
     return allocated;
 }
 
-static void leakTrackingFree(void* p, size_t old) noexcept {
+void __leakTrackingFree(void* p, size_t old) noexcept {
     allocation_mutex.lock_shared();
     allocated_memory.erase(p);
     allocator.deallocate(reinterpret_cast<size_t*>(p), old);
     allocation_mutex.unlock_shared();
 }
 
-static void* leakTrackingRealloc(void* p, size_t old, size_t n) {
+void* __leakTrackingRealloc(void* p, size_t old, size_t n) {
     void* reallocated = nullptr;
     if(n != 0){
-        reallocated = leakTrackingAlloc(n);
+        reallocated = __leakTrackingAlloc(n);
         memcpy(reallocated, p, sizeof(size_t)*std::min(old, n));
     }
-    leakTrackingFree(p, old);
+    __leakTrackingFree(p, old);
     return reallocated;
 }
-
-struct Init {
-    Init(){ mp_set_memory_functions(leakTrackingAlloc, leakTrackingRealloc, leakTrackingFree); }
-};
-
-static Init memoryTrackingInit;
 
 bool isAllGmpMemoryFreed() noexcept {
     return allocated_memory.empty();
--- src/ki_cas_big_num_wrapper.h
+++ src/ki_cas_big_num_wrapper.h
@@ -6,6 +6,29 @@
 #endif
 
 #include <gmp.h>
+
+#ifndef NDEBUG
+namespace KiCAS2 {
+
+bool isAllGmpMemoryFreed() noexcept;
+
+void* __leakTrackingAlloc(size_t n);
+void __leakTrackingFree(void* p, size_t old) noexcept;
+void* __leakTrackingRealloc(void* p, size_t old, size_t n);
+
+struct __Init {
+    __Init(){ mp_set_memory_functions(__leakTrackingAlloc, __leakTrackingRealloc, __leakTrackingFree); }
+};
+
+inline __Init __memoryTrackingInit;
+}
+#define DEBUG_REQUIRE(x) REQUIRE(x)
+#define DEBUG_REQUIRE_FALSE(x) REQUIRE_FALSE(x)
+#else
+#define DEBUG_REQUIRE(x)
+#define DEBUG_REQUIRE_FALSE(x)
+#endif
+
 #include <flint/fmpq.h>
 #include <flint/fmpz.h>
 
@@ -79,15 +102,6 @@ fmpq fmpq_from_decimal_str(std::string_view str, size_t decimal_index);
 /// `['0'-'9']+ ('.' ['0'-'9']*)? 'e' ('+' | '-')? ['0'-'9']+`.
 /// or `'.' ['0'-'9']+ 'e' ('+' | '-')? ['0'-'9']+`
 fmpq fmpq_from_scientific_str(std::string_view str);
-
-#ifndef NDEBUG
-bool isAllGmpMemoryFreed() noexcept;
-#define DEBUG_REQUIRE(x) REQUIRE(x)
-#define DEBUG_REQUIRE_FALSE(x) REQUIRE_FALSE(x)
-#else
-#define DEBUG_REQUIRE(x)
-#define DEBUG_REQUIRE_FALSE(x)
-#endif
 
 }
 
