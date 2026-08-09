0d6862caabdaf5605350343eede5a82a8f1111c2
John Till <JohnDTill@gmail.com>
2026-08-09 02:15:37 +0100

    Remove inline decl

--- include/ki_cas_kmpz.h
+++ include/ki_cas_kmpz.h
@@ -17,9 +17,9 @@ typedef intx::uint128 uint128_t;
 typedef intx::uint256 uint256_t;
 typedef intx::uint512 uint512_t;
 
-template<bool is_negative=false> inline void mpz_init_set_uint128(mpz_t lhs, uint128_t rhs);
-template<bool is_negative=false> inline void mpz_init_set_uint256(mpz_t lhs, uint256_t rhs);
-template<bool is_negative=false> inline void mpz_init_set_uint512(mpz_t lhs, uint512_t rhs);
+template<bool is_negative=false> void mpz_init_set_uint128(mpz_t lhs, uint128_t rhs);
+template<bool is_negative=false> void mpz_init_set_uint256(mpz_t lhs, uint256_t rhs);
+template<bool is_negative=false> void mpz_init_set_uint512(mpz_t lhs, uint512_t rhs);
 template<bool is_negative=false> fmpz u128_to_fmpz(uint128_t val);
 template<bool is_negative=false> fmpz u256_to_fmpz(uint256_t val);
 void write_uint128(std::string& str, uint128_t val);
